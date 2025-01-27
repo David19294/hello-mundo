5e1b09414713942e6ef46081424ddbbbaad7d6a0
David <lopez_david_g@hotmail.com>
2025-01-27 13:08:25 +0000

    primer commit

---
+++ src/ClasePrincipal.java
@@ -0,0 +1,12 @@
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Hola otra vez");
+
+		/*
+		for(String s : args) {
+			System.out.println(s);
+		}*/
+    }
+}
